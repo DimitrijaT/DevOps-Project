cf615969c47537f1d5b6b7075244e8f8638f1392
Димитрија Тимески <32930367+DimitrijaT@users.noreply.github.com>
2023-09-10 19:03:48 +0200

    Bug fix, missing code

--- src/main/java/mk/ukim/finki/kiii/service/CourseService.java
+++ src/main/java/mk/ukim/finki/kiii/service/CourseService.java
@@ -1,2 +1,10 @@
-package mk.ukim.finki.kiii.service.impl;public interface CourseService {
+package mk.ukim.finki.kiii.service;
+
+import mk.ukim.finki.kiii.model.Course;
+
+import java.util.List;
+
+public interface CourseService {
+
+    List<Course> listAll();
 }
--- src/test/java/mk/ukim/finki/kiii/KiiiApplicationTests.java
+++ src/test/java/mk/ukim/finki/kiii/KiiiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class KiiiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
